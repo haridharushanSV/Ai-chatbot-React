7556bf5506e6dbac5f732f00cda8e671e62060a5
svharisaravanan <sharisaravanan@gmail.com>
2025-05-22 21:21:52 +0530

    Text area

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import styles from "./App.module.css";
-
+import Chat from "./components/chats/Chat.jsx";
+import React, { useState } from "react";
 function App() {
+   const [messages, setMessages] = useState(MESSAGES);
   return (
+    
     <div className={styles.App}>
+        
       <header className={styles.Header}>
         <img className={styles.Logo} src="/logo.png" />
         <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
-      <div className={styles.ChatContainer} />
+      <div className={styles.ChatContainer} >
+      <Chat message={messages}/>
+      </div>
     </div>
   );
 }
+
+const MESSAGES = [{
+  role: "user",
+  content: "Hello, how are you?",
+},
+{
+  role: "assistant",
+  content: "I'm fine, thank you! How can I help you today?",
+},
+{
+  role: "user",
+  content: "Can you tell me a joke?",
+},
+{
+  role: "assistant",
+  content: "Sure! Why did the scarecrow win an award? Because he was outstanding in his field!",
+}
+];
 
 export default App;
---
+++ src/components/chats/Chat.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Chat.module.css';
+export default function Chat({ message }) {
+  return (
+    <div className={styles.Chat}>
+    {message.map(({ role, content }, index) => (
+      <div key={index} className={styles.Message} data-role={role}>
+        {content}
+      </div>
+    ))}
+  </div>
+  );
+}
